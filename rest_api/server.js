137a87d0f65e1cf1548faa5c14f8193d4aa1f42c
David Miller <dmiller23@myseneca.ca>
2020-06-07 00:25:19 -0400

    post route added for dataset

--- rest_api/server.js
+++ rest_api/server.js
@@ -19,12 +19,43 @@ var pool = mysql.createPool({
 	}
 });
 
+function base64Encode(file){
+        var bitmap = fs.readFileSync(file);
+        return new Buffer(bitmap).toString('base64');
+}
+
+/* ROUTES */
+
+app.use(express.json());
+
 app.get('/dataset', (req, res)=>{
         pool.query("SELECT * FROM prj566_201a11.dataset", function (err, result, fields){
                 if (err) throw err;
                 res.send(result);
         })
 });
+
+app.post('/dataset', (req, res)=>{
+        console.log(req.body);
+        /*pool.query("", function (err, result, fields){
+                if (err) throw err;
+                res.send(result);
+        })*/
+        //This route will require knowledge of the Item to which it's linked
+        //Necessary info in the request: itemID (or name, which can then perform a GET to /item to tell which itemID to use),
+        //image to upload: filename? already-translated base64 string?
+        //Possible TODO: configure route to be able to handle either file (will contain a period) or base64 string (will not contain a period) - could be handy for Keras
+        //INSERT INTO prj566_201a11.dataset (itemID, image) values(id, base64String)
+        console.log(req.body);
+        var base64String = base64Encode(req.body.imageURI);
+        var id = req.body.id.toString();        //TODO: protect this against sql injection
+        pool.query("INSERT INTO prj566_201a11.dataset (itemID, image) values(" + id + ", '" + base64String + "');", function (err, result, fields){
+                if (err) throw err;
+                res.send(result);
+                res.redirect("/success");
+        })
+        return;
+})
 
 app.get('/item', (req, res)=>{
         pool.query("SELECT * FROM prj566_201a11.item", function (err, result, fields){
@@ -33,12 +64,26 @@ app.get('/item', (req, res)=>{
         })
 });
 
+app.post('/item', (req, res)=>{
+        pool.query("", function (err, result, fields){
+                if (err) throw err;
+                res.send(result);
+        })
+})
+
 app.get('/identifiedobject', (req, res)=>{
         pool.query("SELECT * FROM prj566_201a11.identifiedobject", function (err, result, fields){
                 if (err) throw err;
                 res.send(result);
         })
 });
+
+app.post('/identifiedobject', (req, res)=>{
+        pool.query("", function (err, result, fields){
+                if (err) throw err;
+                res.send(result);
+        })
+})
 
 app.get('/imagepack', (req, res)=>{
         pool.query("SELECT * FROM prj566_201a11.imagepack", function (err, result, fields){
@@ -47,12 +92,26 @@ app.get('/imagepack', (req, res)=>{
         })
 });
 
+app.post('/imagepack', (req, res)=>{
+        pool.query("", function (err, result, fields){
+                if (err) throw err;
+                res.send(result);
+        })
+})
+
 app.get('/matchhistoryitem', (req, res)=>{
         pool.query("SELECT * FROM prj566_201a11.matchhistoryitem", function (err, result, fields){
                 if (err) throw err;
                 res.send(result);
         })
 });
+
+app.post('/matchhistoryitem', (req, res)=>{
+        pool.query("", function (err, result, fields){
+                if (err) throw err;
+                res.send(result);
+        })
+})
 
 app.get('/recyclingmessage', (req, res)=>{
         pool.query("SELECT * FROM prj566_201a11.recyclingmessage", function (err, result, fields){
@@ -61,12 +120,26 @@ app.get('/recyclingmessage', (req, res)=>{
         })
 });
 
+app.post('/recyclingmessage', (req, res)=>{
+        pool.query("", function (err, result, fields){
+                if (err) throw err;
+                res.send(result);
+        })
+})
+
 app.get('/users', (req, res)=>{
         pool.query("SELECT * FROM prj566_201a11.users", function (err, result, fields){
                 if (err) throw err;
                 res.send(result);
         })
 });
+
+app.post('/users', (req, res)=>{
+        pool.query("", function (err, result, fields){
+                if (err) throw err;
+                res.send(result);
+        })
+})
 
 app.get('/validationemail', (req, res)=>{
         pool.query("SELECT * FROM prj566_201a11.validationemail", function (err, result, fields){
@@ -75,8 +148,22 @@ app.get('/validationemail', (req, res)=>{
         })
 });
 
+app.post('/validationemail', (req, res)=>{
+        pool.query("", function (err, result, fields){
+                if (err) throw err;
+                res.send(result);
+        })
+})
+
+
+/********************************************************************/
+
 app.get('/', (request, response)=>{
         response.send('server works');
+});
+
+app.get('/success', (request, response)=>{
+        response.send('request sent successfully');
 });
 
 app.listen(HTTP_PORT, ()=>{
